a5702f60652a742116a0f48de86f03cc9941282a
Hussein-ElAttar <hussien.elattar1@gmail.com>
2021-12-18 05:36:58 +0200

    Add app layout. Remove unused deafult components

---
+++ src/layouts/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="hello">
+    Footer
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  props: {
+  },
+};
+</script>
+<style scoped>
+</style>
---
+++ src/layouts/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a
+            class="nav-link active"
+            aria-current="page"
+          > الرئيسية </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link">التصنيفات</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link">التطبيقات</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link">طور تطبيق</a>
+        </li>
+      </ul>
+      <a class="navbar-brand">
+        <img
+          class="logo-image justify-self-start"
+          height="47"
+          src="@/assets/app-logo.png"
+          alt="image"
+        >
+      </a>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {},
+};
+</script>
+<style scoped>
+</style>
