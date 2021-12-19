b72412329d48b1af0ae81dcbca0358bca525e7d9
Hussein-ElAttar <hussien.elattar1@gmail.com>
2021-12-19 19:27:26 +0200

    Finish footer component

--- src/components/layouts/Footer.vue
+++ src/components/layouts/Footer.vue
@@ -1,5 +1,43 @@
 <template>
-  <div id="footer">Footer</div>
+  <div id="footer">
+    <div class="icon-container">
+      <div class="action-icon">
+        <font-awesome-icon :style="{ color: '#8D5DA7' }" icon="arrow-up" />
+      </div>
+    </div>
+    <div>
+      <div class="logo">
+        <img
+          class="logo-image"
+          src="@/assets/media/app-logo-white.png"
+          alt="image"
+        />
+      </div>
+      <div class="social-links">
+        <font-awesome-icon
+          :style="{ color: 'white' }"
+          :icon="['fab', 'youtube']"
+        />
+        <font-awesome-icon
+          :style="{ color: 'white' }"
+          :icon="['fab', 'linkedin-in']"
+        />
+        <font-awesome-icon
+          :style="{ color: 'white' }"
+          :icon="['fab', 'instagram']"
+        />
+        <font-awesome-icon
+          :style="{ color: 'white' }"
+          :icon="['fab', 'twitter']"
+        />
+      </div>
+    </div>
+    <div class="icon-container">
+      <div class="action-icon">
+        <font-awesome-icon icon="comments" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,9 +46,42 @@ export default {
   props: {},
 };
 </script>
+
 <style scoped lang="scss">
 #footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   background-color: $zid-purple;
   text-align: center;
+  height: 233px;
+
+  .logo-image {
+    width: 250px;
+    height: auto;
+  }
+
+  .icon-container {
+    height: 100%;
+    display: flex;
+    align-items: end;
+  }
+
+  .action-icon {
+    width: 35px;
+    height: 35px;
+    border-radius: 50%;
+    background-color: $zid-yellow;
+    margin: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .social-links {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px;
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,31 @@
-import "bootstrap"
-import "bootstrap/dist/css/bootstrap.min.css"
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faInstagram,
+  faLinkedinIn,
+  faTwitter,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
+import { faArrowUp, faComments } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-Vue.config.productionTip = false
+library.add([
+  faArrowUp,
+  faComments,
+  faLinkedinIn,
+  faInstagram,
+  faYoutube,
+  faTwitter,
+]);
+
+Vue.config.productionTip = false;
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
