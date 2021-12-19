13d3fb8fc265e8ba4eb1fdee497af8bedff6e4e7
Hussein-ElAttar <hussien.elattar1@gmail.com>
2021-12-19 21:19:02 +0200

    Add feature apps home component

---
+++ src/components/home/FeaturedApps.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="featured-apps">
+    <div class="header">
+      <div class="title">اﻟﺘﻄﺒﻴﻘﺎت اﻟﻤﻤﻴﺰة</div>
+      <div class="description">
+        ﺗﻄﺒﻴﻘﺎت ﺗﻘﺪم ﺣﻠﻮل وﺧﺪﻣﺎت ﻣﻤﻴﺰة ﻧﻮﺻﻲ ﺑﺘﺠﺮﺑﺘﻬﺎ
+      </div>
+    </div>
+    <div class="apps row">
+      <div
+        v-for="index in 4"
+        :key="index"
+        class="col-xl-3 col-md-6 d-flex justify-content-center"
+      >
+        <review-card
+          title="قمرة"
+          :stars="5"
+          author="عمر برهوم"
+          comment="نظﺎم اﻟﻮﻟﺎء اﻟﺄﻣﺜﻞ اﻟﺬي ﻳﻤﻜﻨﻚ ﻣﻦ رﻓﻊ ﻣﺒﻴﻌﺎﺗﻚ ﻧﻈﺎم اﻟﻮﻟﺎء اﻟﺄﻣﺜﻞ اﻟﺬي
+        ﻳﻤﻜﻨﻚ ﻣﻦ رﻓﻊ ﻣﺒﻴﻌﺎﺗﻚ"
+        ></review-card>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ReviewCard from "@/components/ui/ReviewCard";
+
+export default {
+  name: "FeaturedApps",
+  components: { ReviewCard },
+  props: {},
+};
+</script>
+
+<style lang="scss">
+#featured-apps {
+  text-align: center;
+}
+.review-card {
+  margin-top: 10px;
+  margin-left: 10px;
+}
+.header {
+  .title {
+    font-family: Cairo;
+    font-size: 24px;
+    font-weight: 600;
+    letter-spacing: -1px;
+    margin-bottom: 5px;
+  }
+  .description {
+    font-family: Cairo;
+    font-size: 14px;
+  }
+}
+.apps {
+  display: flex;
+  justify-content: end;
+  margin: 40px 82px 105px 40px;
+}
+</style>
